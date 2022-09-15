422d635a33b756805b6c7bd56d9843ea80741de2
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-15 07:39:57 +0530

    NEW:Programe for Swaping Two Number using third veriable

---
+++ src/com/bridgelabz/SwapTwoNum.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class SwapTwoNum {
+    public static void main(String[] args) {
+        System.out.print("Enter first number : " );
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a=b;
+        b=temp;
+
+        System.out.println("After swapping numbers, first number is : "+a+" and second number is : "+b );
+
+    }
+}
