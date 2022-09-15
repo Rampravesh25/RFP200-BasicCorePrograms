fae8ad454896838b465d87747091018594da18ae
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-15 07:59:51 +0530

    Added: find number is oddd or even

---
+++ src/com/bridgelabz/OddEven.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int check = sc.nextInt();
+
+        if (check % 2 == 0)
+            System.out.println("number is even");
+        else
+            System.out.println("number is odd");
+    }
+}
+
