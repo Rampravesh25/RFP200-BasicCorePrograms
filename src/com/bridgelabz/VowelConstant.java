67da9e97c61dfb0f96ce932b474dbdb3287743fa
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-15 08:04:43 +0530

    Added: find number is Vowels and Consunant

---
+++ src/com/bridgelabz/VowelConstant.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class VowelConstant {
+    public static void main(String[] args) {
+        System.out.print("Enter an Alphabet : ");
+        Scanner sc = new Scanner(System.in);
+        char check = sc.next().charAt(0);
+
+        if (check == 'a' || check == 'e' || check == 'i' || check == 'o' || check == 'u')
+            System.out.println(check + " is vowel");
+        else
+            System.out.println(check + " is consonant");
+    }
+}
