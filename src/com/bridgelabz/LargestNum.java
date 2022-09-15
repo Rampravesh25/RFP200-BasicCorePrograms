c2df56908789b771e5fa747ba2669d433d6242fa
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-15 08:11:45 +0530

    Added: find number largest num

---
+++ src/com/bridgelabz/LargestNum.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LargestNum {
+    public static void main(String[] args) {
+        System.out.print("Enter Three numbers, give space between each : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.print("Largest number is : ");
+        System.out.print((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+    }
+}
